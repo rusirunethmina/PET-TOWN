82fcb51b87e4acbf90415ef9cf61cad0ed1a855f
Sadeepa894 <70631098+Sadeepa894@users.noreply.github.com>
2021-01-16 21:22:55 +0530

    splash activity

---
+++ SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.videocaller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,LoginActivity.class));
+                finish();
+
+            }
+        },2500);
+    }
+}
